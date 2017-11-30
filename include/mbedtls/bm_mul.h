066a13403318ada84b65f6109ea4efa945e97052
Marco Bodrato <bodrato@mail.dm.unipi.it>
2017-11-30 14:43:40 +0100

    include/mbedtls/bm_mul.h: New macro MULADDC_HUIT (2-way) From : RSA-2048                 :    2000  public/s To   : RSA-2048                 :    2200  public/s

--- include/mbedtls/bm_mul.h
+++ include/mbedtls/bm_mul.h
@@ -20,6 +20,57 @@
             "str    x9, [%1], #8         \n\t"   \
             "cinc   %0, x6, cs           \n\t"
 
+#define MULADDC_HUIT                             \
+            "ldp    x5, x7, [%2], #64    \n\t"   \
+            "ldp    x6, x9, [%1], #64    \n\t"   \
+            "adds   x6, x6, %0           \n\t"   \
+            "umulh  %0, %3, x5           \n\t"   \
+            "mul    x5, %3, x5           \n\t"   \
+            "adcs   x9, x9, %0           \n\t"   \
+            "umulh  %0, %3, x7           \n\t"   \
+            "mul    x7, %3, x7           \n\t"   \
+            "cinc   %0, %0, cs           \n\t"   \
+            "adds   x6, x5, x6           \n\t"   \
+            "adcs   x9, x7, x9           \n\t"   \
+            "stp    x6, x9, [%1, #-64]   \n\t"   \
+            "ldp    x6, x9, [%1, #-48]   \n\t"   \
+            "ldp    x5, x7, [%2, #-48]   \n\t"   \
+            "adcs   x6, x6, %0           \n\t"   \
+            "umulh  %0, %3, x5           \n\t"   \
+            "mul    x5, %3, x5           \n\t"   \
+            "adcs   x9, x9, %0           \n\t"   \
+            "umulh  %0, %3, x7           \n\t"   \
+            "mul    x7, %3, x7           \n\t"   \
+            "cinc   %0, %0, cs           \n\t"   \
+            "adds   x6, x5, x6           \n\t"   \
+            "adcs   x9, x7, x9           \n\t"   \
+            "stp    x6, x9, [%1, #-48]   \n\t"   \
+            "ldp    x6, x9, [%1, #-32]   \n\t"   \
+            "ldp    x5, x7, [%2, #-32]   \n\t"   \
+            "adcs   x6, x6, %0           \n\t"   \
+            "umulh  %0, %3, x5           \n\t"   \
+            "mul    x5, %3, x5           \n\t"   \
+            "adcs   x9, x9, %0           \n\t"   \
+            "umulh  %0, %3, x7           \n\t"   \
+            "mul    x7, %3, x7           \n\t"   \
+            "cinc   %0, %0, cs           \n\t"   \
+            "adds   x6, x5, x6           \n\t"   \
+            "adcs   x9, x7, x9           \n\t"   \
+            "stp    x6, x9, [%1, #-32]   \n\t"   \
+            "ldp    x6, x9, [%1, #-16]   \n\t"   \
+            "ldp    x5, x7, [%2, #-16]   \n\t"   \
+            "adcs   x6, x6, %0           \n\t"   \
+            "umulh  %0, %3, x5           \n\t"   \
+            "mul    x5, %3, x5           \n\t"   \
+            "adcs   x9, x9, %0           \n\t"   \
+            "umulh  %0, %3, x7           \n\t"   \
+            "mul    x7, %3, x7           \n\t"   \
+            "cinc   %0, %0, cs           \n\t"   \
+            "adds   x6, x5, x6           \n\t"   \
+            "adcs   x9, x7, x9           \n\t"   \
+            "stp    x6, x9, [%1, #-16]   \n\t"   \
+            "cinc   %0, %0, cs           \n\t"
+
 #define MULADDC_STOP                             \
          : "+r" (c),  "+r" (d), "+r" (s)         \
          : "r" (b)                               \
