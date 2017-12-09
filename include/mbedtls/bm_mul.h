a527717e48e8d2120215bcb4098f24ff6c46c988
Marco Bodrato <bodrato@mail.dm.unipi.it>
2017-12-09 12:57:53 +0100

    include/mbedtls/bm_mul.h (MULADDC_HUIT): better (maybe +1% speed) schedule

--- include/mbedtls/bm_mul.h
+++ include/mbedtls/bm_mul.h
@@ -28,16 +28,16 @@
             "umulh  %0, %3, x6           \n\t"   \
             "ldp    x7, x9, [%2, #-48]   \n\t"   \
             "mul    x5, %3, x5           \n\t"   \
-            "adcs   x11, x11, x14        \n\t"   \
             "ldp    x12, x13, [%1, #-48] \n\t"   \
+            "mul    x6, %3, x6           \n\t"   \
+            "adcs   x11, x11, x14        \n\t"   \
             "umulh  x14, %3, x7          \n\t"   \
             "adcs   x12, x12, %0         \n\t"   \
             "umulh  %0, %3, x9           \n\t"   \
             "adcs   x13, x13, x14        \n\t"   \
-            "mul    x6, %3, x6           \n\t"   \
+            "mul    x7, %3, x7           \n\t"   \
             "cinc   %0, %0, cs           \n\t"   \
             "adds   x10, x10, x5         \n\t"   \
-            "mul    x7, %3, x7           \n\t"   \
             "adcs   x11, x11, x6         \n\t"   \
             "mul    x9, %3, x9           \n\t"   \
             "stp    x10, x11, [%1, #-64] \n\t"   \
@@ -51,23 +51,23 @@
             "umulh  %0, %3, x6           \n\t"   \
             "ldp    x7, x9, [%2, #-16]   \n\t"   \
             "mul    x5, %3, x5           \n\t"   \
-            "adcs   x11, x11, x14        \n\t"   \
             "ldp    x12, x13, [%1, #-16] \n\t"   \
+            "mul    x6, %3, x6           \n\t"   \
+            "adcs   x11, x11, x14        \n\t"   \
             "umulh  x14, %3, x7          \n\t"   \
             "adcs   x12, x12, %0         \n\t"   \
             "umulh  %0, %3, x9           \n\t"   \
             "adcs   x13, x13, x14        \n\t"   \
-            "mul    x6, %3, x6           \n\t"   \
+            "mul    x7, %3, x7           \n\t"   \
             "cinc   %0, %0, cs           \n\t"   \
             "adds   x10, x10, x5         \n\t"   \
-            "mul    x7, %3, x7           \n\t"   \
             "adcs   x11, x11, x6         \n\t"   \
             "mul    x9, %3, x9           \n\t"   \
             "stp    x10, x11, [%1, #-32] \n\t"   \
             "adcs   x12, x12, x7         \n\t"   \
             "adcs   x13, x13, x9         \n\t"   \
-            "stp    x12, x13, [%1, #-16] \n\t"   \
-/* cinc  */ "cinc   %0, %0, cs           \n\t"
+/* cinc  */ "cinc   %0, %0, cs           \n\t"   \
+            "stp    x12, x13, [%1, #-16] \n\t"
 
 #define MULADDC_STOP                             \
          : "+r" (c),  "+r" (d), "+r" (s)         \
